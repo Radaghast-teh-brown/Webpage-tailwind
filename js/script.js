437cf3f179f0350053f1f2593f3924cc00058a19
Magno Leal de Brito Junior <CIANDT\magnobrito@lnb024243cps.ciandt.global>
2023-06-22 12:01:23 -0300

    javascript interaction

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const input = document.getElementById('link-input');
+const linkForm = document.getElementById('link-form');
+const errMsg = document.getElementById('err-msg');
+
+linkForm.addEventListener('submit', formSubmit);
+
+function formSubmit(e) {
+    e.preventDefault();
+
+    if(input.value === '') {
+        errMsg.innerHTML = 'Please enter something';
+        input.classList.add('bg-black');
+    }
+
+}
